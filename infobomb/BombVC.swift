45172411ac03f98829f7fc536bd03ca2291de81b
samuscarella <stephen.muscarella@kibocommerce.com>
2017-02-09 06:56:39 -0600

    Updated Bomb Init Algorithms

--- infobomb/BombVC.swift
+++ infobomb/BombVC.swift
@@ -110,6 +110,7 @@ class BombVC: UIViewController {
     
     func startGeoFireQuery() {
         
+        print("OMG: \(meters)")
         meters += 1.0
         query = geoFire?.query(at: center, withRadius: Double(meters))
 
